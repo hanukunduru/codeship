bc5519aaa2e8c34b524a659ae5d102f78c9dd6a5
CodeShip <codeship@example.com>
2017-12-18 22:22:20 +0000

    PEP-8 Fix

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 def print_hello:
-	print("This is the new branch")
-	# asdjkfhskajdfhkasjdf kjhasd kjkdfjhaksjdfh kjahsdkjfh askjdfhkjasdf kjsdhf kjashdfk jasdfj kjashdf kjashdf  kajdshfkjhsa dfjk jkhals dfjk asdf
-	""" aksjdfhkjasdhf jksd fkjashdf kjsahdf ja sdkfj jksdhf asdkjfh ksajdfh kjasdf j ajkhdsfkj asdjkfhas dfjkasdf skajdhf aksjdfh asdkjf jkasdhf kasjdhf akjdf ajksdfh asdf
+    print("This is the new branch")
+    # asdjkfhskajdfhkasjdf kjhasd kjkdfjhaksjdfh kjahsdkjfh askjdfhkjasdf
+    # kjsdhf kjashdfk jasdfj kjashdf kjashdf  kajdshfkjhsa dfjk jkhals dfjk
+    # asdf
+    """ aksjdfhkjasdhf jksd fkjashdf kjsahdf ja sdkfj jksdhf asdkjfh ksajdfh kjasdf j ajkhdsfkj asdjkfhas dfjkasdf skajdhf aksjdfh asdkjf jkasdhf kasjdhf akjdf ajksdfh asdf
 	akjs dfkjasdhfkasjdhf ksajd kjashdf kjsahdfkjsadf jk jkasdhf asdfjk """
-
-
-
-
-
