8dccbd5c8c37224209432cef5249c76027ad4cba
Hanu Kunduru <hanu@openinvest.co>
2017-12-18 13:48:38 -0800

    test2

--- pep8.sh
+++ pep8.sh
@@ -3,6 +3,7 @@ num_errors_before=`find . -name \*.py -exec pep8 --ignore=E402 {} + | wc -l`
 echo $num_errors_before
 git config --global user.email "codeship@example.com"
 git config --global user.name "CodeShip"
+git checkout -b master origin/master
 find . -name \*.py -exec autopep8 --recursive --aggressive --aggressive --in-place {} +
 num_errors_after=`find . -name \*.py -exec pep8 --ignore=E402 {} + | wc -l`
 echo $num_errors_after
