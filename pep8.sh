92657df6e6abb330cf238523473b5e9ac9b91c2e
Hanu Kunduru <hanu@openinvest.co>
2017-12-18 11:44:55 -0800

    try to attach detached head

--- pep8.sh
+++ pep8.sh
@@ -8,7 +8,11 @@ num_errors_after=`find . -name \*.py -exec pep8 --ignore=E402 {} + | wc -l`
 echo $num_errors_after
 if (( $num_errors_after < $num_errors_before )); then
     git commit -a -m "PEP-8 Fix"
-    git config --global push.default current # Push only to the current branch.
-    git push origin master --quiet
+    git checkout –b temp #makes a new branch from current detached HEAD
+    git branch –f master temp #update master to point to the new <temp> branch
+    git branch –d temp #delete the <temp> branch
+    git push origin master
+    git config --global push.default simple # Push only to the current branch.
+    git push --quiet
 fi
 find . -name \*.py -exec pep8 --ignore=E402 {} +
